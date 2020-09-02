dc0357953c62c9836d608d424711def7a291151f
RienPost <rien@rienpost.nl>
2020-09-02 14:23:42 +0200

    taak1 arrays

---
+++ 1-Arrays/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $studenten = array("Piet ", "Muhammad", "Els");
+
+        $voornaam = array("Piet ", "Muhammad", "Els");
+        $achternaam = array("Post ", "Ali", "Van Olderen");
+        $leeftijd = array("18 ", "17", "16");
+        $klas = array("9g ", "9g", "9g");
+
+        echo "Voornaam: ". $voornaam['0'] . "Achternaam: ". $achternaam['0'] . "Leeftijd: ". $leeftijd['0'] . "Klas: ". $klas['0'];
+
+
+
+
+    ?>
+
+
+
+</body>
+
+</html>
+
