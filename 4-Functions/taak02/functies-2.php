d37c2c7b361a1f44402ec5000efc3ef8b1c34eab
Rien Post <Rien@RP.Local>
2020-09-05 12:47:17 +0200

    Functions taak 2 niet gelukt

---
+++ 4-Functions/taak02/functies-2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $sumarray = array(1, 1, 1);
+        $getal1 = 1;
+        $getal2 = 1;
+        $getal3 = 1;
+
+        function rekenUit($getal1, $getal2, $getal3){
+            echo "als ik de getallen $getal1, $getal2 en $getal3 bij elkaar optel, dan krijg ik $sumarray";
+        }
+
+        
+
+    ?>
+</body>
+</html>
