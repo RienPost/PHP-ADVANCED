2687496ed88e5001cbb0be087ca4a37b854255ae
Rien Post <Rien@RP.Local>
2020-09-05 12:17:57 +0200

    Functions taak 1

---
+++ 4-Functions/taak01/functies-1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        function schrijfKleur($kleur){
+            $kleur;
+        }
+        
+        schrijfkleur(print "<font color=\"red\">rood</font><br/>");
+
+        schrijfkleur(print "<font color=\"yellow\">geel</font><br/>");
+
+        schrijfkleur(print "<font color=\"blue\">blauw</font><br/>");
+
+        schrijfkleur(print "<font color=\"green\">groen</font><br/>");
+
+    ?>
+</body>
+</html>
